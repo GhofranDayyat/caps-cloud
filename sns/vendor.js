76366121f368e8c38e249025a09539c6f2527f5a
ghofrandayyat <ghofrandayyat@gmail.com>
2021-06-03 17:47:42 +0300

    done

--- sns/vendor.js
+++ sns/vendor.js
@@ -1,37 +1,81 @@
-'use strict';
-
+// you should re-change your creadintioal each session
 const AWS = require('aws-sdk');
-// AWS.config.update({region: 'us-east-1'});
-AWS.config.update({region: 'us-east-1'})
-var sns = new AWS.SNS();
+const uuid = require('uuid').v4;
+const faker = require('faker');
 
-  
-const topicArnt = 'arn:aws:sqs:us-east-1:804075518076:firtQ' 
+AWS.config.update({region:'us-east-1'}); // from V.virginia
 
-const msg= JSON.stringify({ orderId: 1234, customer: "Jane Doe", vendorId: topicArnt});
+const sns = new AWS.SNS(); 
 
-const payload= {
-    Message: msg,
-    TopicArn:topicArnt
+const topicArn = 'arn:aws:sns:us-east-1:804075518076:firstT';
+const msg = { 
+  orderId: uuid(),
+  customer: faker.name.findName(), 
+  vendorId: 'arn:aws:sns:us-east-1:804075518076:firstT'
 }
 
-sns.publish({
-    Message: msg,
-    TopicArn:topicArnt
-}).promise()
-  .then(data => {
-      console.log('333333333333');
-    console.log('helo',data);
-  }).catch(error=>{
-      console.log(error.Message);
-  });
-;
-// var returnValue = sns.publish({
-//     Message: msg,
-//     TopicArn:topicArnt
-// })
-// returnValue.on('complete', function() {
-//     console.log(returnValue.params);
-//   }).send();
+const payload = {
+  Message: JSON.stringify(msg),
+  TopicArn: topicArn,
+};
 
-    // console.log(returnValue.params);
+sns.publish(payload).promise()
+    .then(data => {
+      console.log('Order sent to driver ------> \n', data);
+    })
+    .catch(console.error);
+
+
+
+// const AWS = require('aws-sdk');
+// const uuid = require('uuid').v4;
+// const faker = require('faker')
+
+// AWS.config.update({region:'us-east-1'});
+
+// const sns = new AWS.SNS();
+
+// const topic = 'arn:aws:sns:us-east-1:804075518076:firstT';
+
+// setInterval(() => {
+//   const message = {
+//     orderId: uuid(),
+//     customer: faker.name.findName(),
+//     vendorId: 'arn:aws:sns:us-east-1:804075518076:firstT'
+//   }
+
+//   const payload = {
+//     Message: JSON.stringify(message),
+//     TopicArn: topic,
+//   };
+
+//   sns.publish(payload).promise()
+//     .then(data => {
+//       console.log('Order sent to driver ------> \n', data);
+//     })
+//     .catch(console.error);
+// }, 5000);
+
+///////////////////////////////////////////////
+
+// // receive delivery acknowledgement
+// const { Consumer } = require('sqs-consumer');
+
+// const app = Consumer.create({
+//   queueUrl: 'https://sqs.us-east-1.amazonaws.com/804075518076/firtQ',
+//   handleMessage: handler,
+// });
+
+// function handler(message) {
+//   console.log(message.Body);
+// }
+
+// app.on('error', (err) => {
+//   console.error(err.message);
+// });
+
+// app.on('processing_error', (err) => {
+//   console.error(err.message);
+// });
+
+// app.start();
--- sqs/driver.js
+++ sqs/driver.js
@@ -1,23 +1,60 @@
-const { Consumer } = require('sqs-consumer');
+// const { Consumer } = require('sqs-consumer');
+
+// // produce messages to the vendor queue
+
+// const uuid = require('uuid').v4;
+// const { Producer } = require('sqs-producer');
+
+// const producer = Producer.create({
+//   queueUrl: `https://sqs.us-east-1.amazonaws.com/804075518076/firtQ`,
+//   region: `us-east-1`,
+// });
+
+// let counter = 0;
+// ///////////////////////////////////////
  
-const app = Consumer.create({
-  queueUrl: 'https://sqs.us-east-1.amazonaws.com/804075518076/firtQ',
-  handleMessage: async (msg) => {
-    // do some work with `message`
-    let parsedBody = JSON.parse(msg.Body);
-    let myOrder = JSON.parse(parsedBody.Message);
-    console.log("text is =", myOrder.orderItem);
-    console.log("store is =", myOrder.storeName);
-  }
-});
+// const app = Consumer.create({
+//   queueUrl: 'https://sqs.us-east-1.amazonaws.com/804075518076/firtQ',
+//   handleMessage: async (msg) => {
+//     // do some work with `message`
+//     let parsedBody = JSON.parse(msg.Body);
+//     let myOrder = JSON.parse(parsedBody.Message);
+//     console.log('!!!! Received order !!!!', myOrder);
+//     // console.log("text is =", myOrder.orderItem);
+//     // console.log("store is =", myOrder.storeName);
+
+//     // creating delivery acknowledgement in vendor queue
+//     await acknowledgement();
+//   }
+// });
  
 
-app.on('error', (err) => {
-  console.error(err.message);
-});
+// app.on('error', (err) => {
+//   console.error(err.message);
+// });
  
-app.on('processing_error', (err) => {
-  console.error(err.message);
-});
+// app.on('processing_error', (err) => {
+//   console.error(err.message);
+// });
  
-app.start();
+// app.start();
+
+// ///////////////////////////////////
+
+// function acknowledgement() {
+//   setTimeout(async () => {
+  
+//     try {
+//       const message = {
+//         id: uuid(),
+//         body: `Message #${counter++}. Delivery successful!}`,
+//       };
+  
+//       const response = await producer.send(message);
+//       // console.log(response);
+//       console.log(':::: Acknowledgement sent to vendor ::::');
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }, Math.floor(Math.random() * 5000));
+// }
